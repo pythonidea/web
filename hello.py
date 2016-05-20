a4991a9e97ad544bd6a4dce78ca3c4aec57cdc00
pythonidea <pythonidea@users.noreply.github.com>
2016-05-20 10:27:08 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,10 @@
 import urlparse
 def wsgi_application (environ, start_responce):
   status = '200 OK'
-  headers = [
-    ('Content-Type', 'text/plain')
-  ]
+  headers = [('Content-Type', 'text/plain')]
   body = ''
-  params = urlparse_qs(environ["QUERY_STRING"]
+  params = urlparse_qs(environ["QUERY_STRING"])
   start_response (status, headers)
   for i in params:
-    body += i[0] + '='+i[1] + '\n'
+    body += i[0] + '=' + i[1] + '\n'
   return [ body ]
